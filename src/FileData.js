9ed98c6aea5d5d5b9fb606939663b02848da34c1
hiteshghodki <hiteshkumarghodki16@gmail.com>
2025-03-06 20:27:55 +0530

    formate code

--- src/FileData.js
+++ src/FileData.js
@@ -1,46 +1,42 @@
 const FileSystem = [
-    {
-        id:1,
-        name: 'Documents',
-        type : 'folders',
-        children:[
-            {
-                id:2,
-                name:'image',
-                type:'file',
-                content: 'Please check the file data'
-            },
-            {
-                id :3,
-                name: "doc",
-                type:'file',
-                content:'please open the file data'
-            }
-        ]
-
-    },
-    {
-        id:4,
-        name: 'Data',
-        type : 'folders',
-        children:[
-            {
-                id:5,
-                name:'text',
-                type:'file',
-                content: 'We can check the file data'
-            },
-            {
-                id :6,
-                name: "doc",
-                type:'file',
-                content:'We can open the file data'
-            }
-        ]
-
-    }
-
-]
-
+  {
+    id: 1,
+    name: "Documents",
+    type: "folders",
+    children: [
+      {
+        id: 2,
+        name: "image",
+        type: "file",
+        content: "Please check the file data",
+      },
+      {
+        id: 3,
+        name: "doc",
+        type: "file",
+        content: "please open the file data",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Data",
+    type: "folders",
+    children: [
+      {
+        id: 5,
+        name: "text",
+        type: "file",
+        content: "We can check the file data",
+      },
+      {
+        id: 6,
+        name: "doc",
+        type: "file",
+        content: "We can open the file data",
+      },
+    ],
+  },
+];
 
 export default FileSystem;
